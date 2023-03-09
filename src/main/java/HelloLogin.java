ca0d07e86e0723728caea3e15bb284b7fd4a367a
Chase Medford <chasemedford1@gmail.com>
2023-03-09 09:09:53 -0600

    Started servlet

---
+++ src/main/java/HelloLogin.java
@@ -0,0 +1,15 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/login")
+public class HelloLogin extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        PrintWriter out = res.getWriter();
+        out.println("<h1 style='color: blue'>Login Page</h1>");
+    }
+}
--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+//User goes to http://localhost:8080/hello in the browser and sees "Hello World"
+
+@WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        System.out.println("Hello World");
+        PrintWriter out = res.getWriter();
+        out.println("<h1 style='color: blue'>Hello World</h1><h3>Look, a smaller heading</h3>");
     }
 }
